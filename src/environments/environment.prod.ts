e7700be2921ffc5874d5d1b6b9d58a2e849671d1
Clemo97 <lumumbaclement@gmail.com>
2023-03-11 14:55:05 +0300

    add api keys and endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
   production: true
+  apiUrl: 'https://api.openweathermap.org/data/2.5/',
+  apiKey: '1566d1b961c0ada54ed17885d9d230ba'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,8 @@
 
 export const environment = {
   production: false
+  apiUrl: 'https://api.openweathermap.org/data/2.5/',
+  apiKey: '1566d1b961c0ada54ed17885d9d230ba'
 };
 
 /*
